52f02bb6acb77a4fb9f84f7c7b1f1962bccd122b
Ricardo Riogo <ricardoriogo@gmail.com>
2015-05-23 18:48:29 -0300

    Initial Commit

---
+++ bootstrap-dropdown-filter.js
@@ -0,0 +1,52 @@
+/**
+ * Input filter for Bootstrap Dropdown
+ * @param  {jQuery} $ Global jQuery instance
+ * @return {void}
+ *
+ * @todo Navigation with the keyboard Up and Down
+ * @todo Display Not Found msg to user
+ * @todo Translate for other languages
+ * ------------------------------------------------------
+ * @done Auto-focus on input when dropdown is opened
+ * @done Add scrollbar to large lists
+ */
+(function( $ ) {
+
+	$.fn.bsDropDownFilter = function(options) {
+
+		return this.filter(".dropdown-menu").each(function() {
+			$this = $(this).css({
+				'overflow-x': 'auto',
+				'max-height': 450
+			});
+
+			$this.parent().on('shown.bs.dropdown', function(e){
+				$(this).find('.dropdown-filter input').focus();
+			});
+
+			$li = $('<li role="presentation" class="dropdown-filter"></li>').prependTo($this);
+
+			$search = $('<input type="search" class="form-control" placeholder="Filtrar por:" style="width:96%; margin:0 auto" />')
+				.data('dropdownList', $this)
+				.bind('click', function(e){
+					e.stopPropagation();
+				})
+				.bind('keyup', function(){
+					$droplist = $(this).data('dropdownList');
+					$droplist.find('li').show();
+					$droplist.find('li:not(:filter("' + this.value + '"))').not('.dropdown-filter').hide();
+				})
+				.prependTo($li);
+		});
+	};
+
+	$('[data-filter], .dropdown-filter').bsDropDownFilter();
+
+	// Create a FILTER pseudo class. Like CONTAINS, but case insensitive
+	$.expr[":"].filter = $.expr.createPseudo(function(arg) {
+		return function( elem ) {
+			return Diacritics.clean($(elem).text()).toUpperCase().indexOf(Diacritics.clean(arg).toUpperCase()) >= 0;
+		};
+	});
+
+}( jQuery ));
